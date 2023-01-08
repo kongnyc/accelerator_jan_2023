f86a9482c688d4748ce10e5be2f76eb29460b859
kongnyc <kongnyc>
2023-01-07 23:23:49 -0500

    method get post params

--- 2023-01-04/studentAppStarter/app.js
+++ 2023-01-04/studentAppStarter/app.js
@@ -18,10 +18,26 @@ const app = express();
 app.use(express.json()); // Why do we need this?
 
 // Route handler functions
-app.get('/', (request, response) => {
-  console.log(request, response);
-
-  response.json({ message: 'Service is running' });
+// app.get('/', (request, response) => {
+//   console.log(request, response);
+//   console.log(request.method, request.url);
+//   response.json({ message: 'Service is running on get main page' });
+// });
+// app.get('*', (request, response) => {
+//   console.log(request, response);
+//   console.log(request.path, request.url);
+//   response.json({ message: 'Service is running on get main page' });
+// });
+// app.post('/studentsDB', (request, response) => {
+//   console.log(request, response);
+//   console.log(request.path, request.body);
+//   response.json({ message: 'Service is running on post' });
+// });
+app.get('/studentsDB/:id', (request, response) => {
+  // console.log(request, response);
+  //http://localhost:9000/studentsDB/2?name=kong&age=32&sex=male
+  console.log(request.params, request.query);
+  response.json({ message: 'Service is running on post' });
 });
 
 module.exports = app;
